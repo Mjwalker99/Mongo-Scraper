4c2d1bff815ca56c329f593dcf6e43ca784224c2
Micah Walker <minimelon19@gmail.com>
2019-01-04 14:52:07 -0600

    commit

---
+++ config/config.js
@@ -0,0 +1,11 @@
+{
+    _id = "heroku_0bwzmj44.MongoScraper123",
+    user = "MongoScraper123",
+    db = "heroku_0bwzmj44",
+    roles = [
+        {
+            role: "dbOwner",
+            db: "heroku_0bwzmj44"
+        }
+    ]
+}

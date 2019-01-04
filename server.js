dcc4dd64950a9d9212708ad794a193fd0b386572
Micah Walker <minimelon19@gmail.com>
2019-01-04 14:43:22 -0600

    mlab

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ const router = require("./controllers/api.js");
 app.use(router);
 // Connect to the Mongo DB
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 
 
